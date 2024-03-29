f1cbd3fe2412ab5a529c58c3598fbc29a67acbab
tmakukha <makukhatb@gmail.com>
2024-03-29 20:56:24 +0500

    update

--- src/controllers/CalcPageController.java
+++ src/controllers/CalcPageController.java
@@ -14,7 +14,28 @@ public class CalcPageController extends BaseController{
     
     @FXML
     private Label totalPriceLabel;
-
+    
+    @FXML
+    private Label lengthLabel;
+    
+    @FXML
+    private Label widthLabel;
+    
+    @FXML
+    private Label floorsCountLabel;
+    
+    @FXML
+    private Label squareLabel;
+    
+    @FXML 
+    private Label roofLabel;
+    
+    @FXML
+    private Label wallLabel;
+    
+    @FXML
+    private Label foundationLabel;
+    
     @FXML
     private void initialize() {
     	// listener exit button
@@ -24,6 +45,33 @@ public class CalcPageController extends BaseController{
         initializeGoBackButton(goBackBtn, "/view/HomePage.fxml", goBackBtn);
     }
     
+    public void updateLengthOutput(double length) {
+    	lengthLabel.setText(String.format("Длина: %.2f", length));
+    }
+    
+    public void updateWidthOutput(double width) {
+    	widthLabel.setText(String.format("Ширина: %.2f", width));
+    }
+    
+    public void updateFloorsCountOutput(int floorsCount) {
+    	floorsCountLabel.setText(String.format("Количество этажей: %d", floorsCount));
+    }
+    
+    public void updateSquareOutput(double square) {
+    	squareLabel.setText(String.format("Площадь фундамента: %.2f", square));
+    }
+    
+    public void updateRoofOutput(String roofType) {
+    	roofLabel.setText(String.format("Тип крыши: %s", roofType));
+    }
+    
+    public void updateWallOutput(String wallType) {
+    	wallLabel.setText(String.format("Тип стен: %s", wallType));
+    }
+    
+    public void updateFoundationOutput(String foundationType) {
+    	foundationLabel.setText(String.format("Тип фундамента: %s", foundationType));
+    }
     // set total price from home page calculate method
     public void updateTotalPrice(double totalPrice) {
         totalPriceLabel.setText(String.format("Итоговая сумма: %.2f", totalPrice));
